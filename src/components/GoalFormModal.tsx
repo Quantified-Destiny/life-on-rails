39b8f63f8961f1a8f3f55502f178d92709c66929
Brian Ha <26440404+beehaa@users.noreply.github.com>
2023-03-07 00:15:15 -0800

    added goal form modal

---
+++ src/components/GoalFormModal.tsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+
+const GoalFormModal = ({ onClose}) => {
+    // const [isModalOpen, setIsModalOpen] = useState(false);
+
+    // const handleModalToggle = () => {
+    //     setIsModalOpen(!isModalOpen);
+    //   };
+    const [goalName, setGoalName] = useState("");
+  const [goalDescription, setGoalDescription] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // TODO: handle form submission
+  };
+
+
+return (
+    <div className="fixed z-10 inset-0 overflow-y-auto bg-black bg-opacity-40">
+    <div className="flex items-center justify-center min-h-screen">
+        {/* <div className="bg-white rounded-lg p-8">
+        <h2 className="text-lg font-medium mb-4">Create Goal</h2>
+        <form>
+            <label className="block mb-2">
+            <span className="text-gray-700">Name</span>
+            <input type="text" name="name" className="form-input mt-1 block w-full" />
+            </label>
+            <label className="block mb-2">
+            <span className="text-gray-700">Description</span>
+            <textarea name="description" className="form-textarea mt-1 block w-full"></textarea>
+            </label>
+            <button type="submit" className="bg-green-600 text-white rounded-md py-2 px-4 hover:bg-green-700">
+            Create Goal
+            </button>
+        </form>
+        </div> */}
+        <div className="mx-auto max-w-xl border border-gray-200 px-4 py-6 sm:px-6 lg:px-8 bg-white">
+  <div className="mb-8 ">
+    <h2 className="mb-2 text-2xl font-semibold text-center">Create Goal</h2>
+    <form className="flex flex-col space-y-4">
+      <div className="my-3 mx-2">
+        <label className="mb-2 block text-sm font-bold text-gray-700" htmlFor="title"> Title </label>
+        <input className="text-sm focus:shadow-outline w-full appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow-sm focus:outline-none" 
+        id="goalName" 
+        type="text" 
+        placeholder="Enter goal title" 
+        onChange={(e) => setGoalName(e.target.value)}
+        required/>
+      </div>
+
+      <div className="my-3 mx-2">
+        <label className="mb-2 block text-sm font-bold text-gray-700" htmlFor="description"> Description </label>
+        <textarea 
+        name="description" 
+        id="goalDescription" 
+        className="text-sm focus:shadow-outline w-full appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow-sm focus:outline-none" 
+        placeholder="Enter goal description" 
+        rows="3" 
+        onChange={(e) => setGoalDescription(e.target.value)}
+        required></textarea>
+      </div>
+
+      <div className="flex justify-end">
+        <button type="button" className="text-sm mr-2 inline-flex items-center rounded-md border border-transparent bg-gray-600 px-4 py-2 font-semibold text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2"
+        onClick={onClose}>Cancel</button>
+        
+        <button type="submit" className="text-sm inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 font-semibold text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Create Goal</button>
+      </div>
+    </form>
+  </div>
+</div>
+
+    </div>
+    </div>
+);
+};
+
+export default GoalFormModal;
--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -1,30 +1,53 @@
 
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import classNames from "classnames";
 import { useState } from "react";
 import Layout from "../components/layout";
 import { api } from "../utils/api";
-const JournalPage = () => {
-    return(
-<div className="mx-auto max-w-xl border border-gray-200 px-4 py-6 sm:px-6 lg:px-8">
-  <h2 className="mb-8 text-2xl font-semibold">Choose an option:</h2>
-  <div className="flex flex-col space-y-4">
-    <button type="button" className="inline-flex items-center rounded-md border border-transparent bg-green-600 px-4 py-2 font-semibold text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2">
-      <span className="mr-2"><i className="fas fa-bullseye"></i></span>
-      <span>Create Goal</span>
-    </button>
-    <button type="button" className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 font-semibold text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-      <span className="mr-2"><i className="fas fa-tasks"></i></span>
-      <span>Create Subjective</span>
-    </button>
-    <button type="button" className="inline-flex items-center rounded-md border border-transparent bg-yellow-600 px-4 py-2 font-semibold text-white hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-offset-2">
-      <span className="mr-2"><i className="fas fa-sticky-note"></i></span>
-      <span>Create Habit</span>
-    </button>
-  </div>
-</div>
-);
+
+import GoalFormModal from "../components/GoalFormModal";
+
+
+
+const CreatePage = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const handleModalToggle = () => {
+        setIsModalOpen(!isModalOpen);
+    };
+
+    return (<>
+        {isModalOpen && (
+            <GoalFormModal
+                onClose={() => {
+                    setIsModalOpen(false);
+                }}
+            />
+        )}
+
+        <div className="mx-auto max-w-xl border border-gray-200 px-4 py-6 sm:px-6 lg:px-8">
+            <h2 className="mb-8 text-2xl font-semibold">Choose an option:</h2>
+            <div className="flex flex-col space-y-4">
+                <button
+                    type="button"
+                    className="inline-flex items-center rounded-md border border-transparent bg-green-600 px-4 py-2 font-semibold text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2"
+                    onClick={handleModalToggle}>
+                    <span className="mr-2">
+                        <i className="fas fa-bullseye"></i>
+                    </span>
+                    <span>Create Goal</span>
+                </button>
+                <button type="button" className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 font-semibold text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                    <span className="mr-2"><i className="fas fa-tasks"></i></span>
+                    <span>Create Subjective</span>
+                </button>
+                <button type="button" className="inline-flex items-center rounded-md border border-transparent bg-yellow-600 px-4 py-2 font-semibold text-white hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-offset-2">
+                    <span className="mr-2"><i className="fas fa-sticky-note"></i></span>
+                    <span>Create Habit</span>
+                </button>
+            </div>
+        </div>
+    </>
+    );
 };
 
-export default () => <Layout main={JournalPage}></Layout>;
+export default () => <Layout main={CreatePage}></Layout>;
