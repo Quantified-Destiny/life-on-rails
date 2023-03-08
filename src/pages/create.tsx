74ca87a3b1ea6025740015a91dca00a8bdeb625f
Brian Ha <26440404+beehaa@users.noreply.github.com>
2023-03-07 23:45:07 -0800

    handled all modal toggles

--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -5,24 +5,39 @@ import { useState } from "react";
 import Layout from "../components/layout";
 import { api } from "../utils/api";
 
-import GoalFormModal from "../components/GoalFormModal";
-
-
+import GoalFormModal from "../components/goalformmodal";
+import HabitFormModal from "../components/habitformmodal";
+import SubjectiveFormModal from "../components/subjectiveformmodal";
 
 const CreatePage = () => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const handleModalToggle = () => {
-        setIsModalOpen(!isModalOpen);
-    };
+    const [isGoalModalOpen, setIsGoalModalOpen] = useState(false);
+  const [isSubjectiveModalOpen, setIsSubjectiveModalOpen] = useState(false);
+  const [isHabitModalOpen, setIsHabitModalOpen] = useState(false);
 
-    return (<>
-        {isModalOpen && (
-            <GoalFormModal
-                onClose={() => {
-                    setIsModalOpen(false);
-                }}
-            />
-        )}
+  const handleGoalModalToggle = () => {
+    setIsGoalModalOpen(!isGoalModalOpen);
+  };
+
+  const handleSubjectiveModalToggle = () => {
+    setIsSubjectiveModalOpen(!isSubjectiveModalOpen);
+  };
+
+  const handleHabitModalToggle = () => {
+    setIsHabitModalOpen(!isHabitModalOpen);
+  };
+
+  return (
+    <>
+      {isGoalModalOpen && (
+        <GoalFormModal onClose={() => setIsGoalModalOpen(false)} />
+      )}
+      {isSubjectiveModalOpen && (
+        <SubjectiveFormModal onClose={() => setIsSubjectiveModalOpen(false)} />
+      )}
+      {isHabitModalOpen && (
+        <HabitFormModal onClose={() => setIsHabitModalOpen(false)} />
+      )}
+
 
         <div className="mx-auto max-w-xl border border-gray-200 px-4 py-6 sm:px-6 lg:px-8">
             <h2 className="mb-8 text-2xl font-semibold">Choose an option:</h2>
@@ -30,17 +45,19 @@ const CreatePage = () => {
                 <button
                     type="button"
                     className="inline-flex items-center rounded-md border border-transparent bg-green-600 px-4 py-2 font-semibold text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2"
-                    onClick={handleModalToggle}>
+                    onClick={handleGoalModalToggle}>
                     <span className="mr-2">
                         <i className="fas fa-bullseye"></i>
                     </span>
                     <span>Create Goal</span>
                 </button>
-                <button type="button" className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 font-semibold text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                <button type="button" className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 font-semibold text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                onClick={handleSubjectiveModalToggle}>
                     <span className="mr-2"><i className="fas fa-tasks"></i></span>
                     <span>Create Subjective</span>
                 </button>
-                <button type="button" className="inline-flex items-center rounded-md border border-transparent bg-yellow-600 px-4 py-2 font-semibold text-white hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-offset-2">
+                <button type="button" className="inline-flex items-center rounded-md border border-transparent bg-yellow-600 px-4 py-2 font-semibold text-white hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-offset-2"
+                onClick={handleHabitModalToggle}>
                     <span className="mr-2"><i className="fas fa-sticky-note"></i></span>
                     <span>Create Habit</span>
                 </button>
