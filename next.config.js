ae7b3e92331257c492f22993749bd2e66c5bc826
Sumeet Padavala <sumeet.padavala@gmail.com>
2023-04-25 19:11:52 -0700

    Fix type issues

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  * This is especially useful for Docker builds.
  */
-!process.env.SKIP_ENV_VALIDATION && (await import("./src/env/server.mjs"));
+!process.env.SKIP_ENV_VALIDATION && import("./src/env/server.mjs");
 
 /** @type {import("next").NextConfig} */
 const config = {
@@ -19,4 +19,5 @@ const config = {
     ignoreDuringBuilds: true,
   },
 };
-export default config;
+
+module.exports = config;
--- src/components/time-picker.tsx
+++ src/components/time-picker.tsx
@@ -1,7 +1,7 @@
 import {
-    faChevronLeft,
-    faChevronRight,
-    faPlus,
+  faChevronLeft,
+  faChevronRight,
+  faPlus,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // fixes zoomed in icons
@@ -16,52 +16,59 @@ const LeftChevron = () => <FontAwesomeIcon icon={faChevronLeft} />;
 const RightChevron = () => <FontAwesomeIcon icon={faChevronRight} />;
 
 interface TimePickerProps {
-    date: Date;
-    setDate: (date: Date) => void;
+  date: Date;
+  setDate: (date: Date) => void;
 }
 
 const today = new Date();
-const maxDate = new Date(today.getFullYear(), today.getMonth(), today.getDate());
-
+const maxDate = new Date(
+  today.getFullYear(),
+  today.getMonth(),
+  today.getDate()
+);
 
 const TimePicker = ({ date, setDate }: TimePickerProps) => {
-    const handlePrevDay = () => {
-        setDate(subDays(date, 1));
-    };
+  const handlePrevDay = () => {
+    setDate(subDays(date, 1));
+  };
 
-    const handleNextDay = () => {
-        setDate(addDays(date, 1));
-    };
+  const handleNextDay = () => {
+    setDate(addDays(date, 1));
+  };
 
-    const showNextDayButton = date.toDateString() !== maxDate.toDateString();
+  const showNextDayButton = date.toDateString() !== maxDate.toDateString();
 
-    return (
-        <div
-            id="time-selector"
-            className="mt-3 flex h-10 w-full flex-col items-center pt-1"
+  return (
+    <div
+      id="time-selector"
+      className="mt-3 flex h-10 w-full flex-col items-center pt-1"
+    >
+      <div id="selector-controls" className="flex items-center">
+        <button onClick={handlePrevDay} className="bg-white bg-opacity-20 px-2">
+          <LeftChevron></LeftChevron>
+        </button>
+        <DatePicker
+          selected={date}
+          onChange={(date) => date && setDate(date)}
+          maxDate={maxDate}
+          className="text-center"
+        />
+
+        <button
+          onClick={handleNextDay}
+          className={`bg-white bg-opacity-20 px-2 ${
+            date >= maxDate ? "cursor-default opacity-50" : ""
+          }`}
+          disabled={date >= maxDate}
         >
-            <div id="selector-controls" className="flex items-center">
-                <button onClick={handlePrevDay} className="bg-white bg-opacity-20 px-2">
-                    <LeftChevron></LeftChevron>
-                </button>
-                <DatePicker
-                    selected={date}
-                    onChange={(date) => setDate(date)}
-                    maxDate={maxDate}
-                    className="text-center" />
-
-                <button
-                    onClick={handleNextDay}
-                    className={`bg-white bg-opacity-20 px-2 ${date >= maxDate && "cursor-default opacity-50"}`}
-                    disabled={date >= maxDate}
-                >
-                    <FontAwesomeIcon icon={faChevronRight} color={date >= maxDate ? "#ccc" : "#000"} />
-                </button>
-            </div>
-        </div>
-
-
-    );
+          <FontAwesomeIcon
+            icon={faChevronRight}
+            color={date >= maxDate ? "#ccc" : "#000"}
+          />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default TimePicker;
