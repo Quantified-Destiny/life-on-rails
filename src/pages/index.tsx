000f63dc87573dd71dfab836feffc74bbd64126c
Brian Ha <26440404+beehaa@users.noreply.github.com>
2023-04-28 01:54:18 -0700

    changed landing page and login ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ import { api } from "../utils/api";
 import { useRouter } from "next/router";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  // const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
   return (
     <>
@@ -16,20 +16,27 @@ const Home: NextPage = () => {
         <meta name="description" content="" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#5215a8] to-[#2b33d7]">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Life <span className="text-[hsl(280,100%,70%)]">on</span> Rails
-          </h1>
+      
 
-          <div className="flex flex-col items-center gap-2">
-            {/* <p className="text-2xl text-white">
-              {hello.data ? hello.data.greeting : "Loading tRPC query..."}
-            </p> */}
-            <AuthShowcase />
-          </div>
-        </div>
-      </main>
+      <section className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#5ef4ca5a] to-[#4389faab] bg-white dark:bg-gray-900">
+  <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
+    <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+    Ride the rails to success
+    </h1>
+    <p className="mb-8 text-lg font-normal text-whitelg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">
+    Empower your journey to success with Life on Rails - the app that tracks your goals, habits, and progress.
+    </p>
+    <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+      <AuthShowcase />
+      <a
+        href="#"
+        className="inline-flex bg-gray-200 justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+      >
+        Learn more
+      </a>
+    </div>
+  </div>
+</section>
     </>
   );
 };
@@ -52,19 +59,35 @@ const AuthShowcase: React.FC = () => {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {secretMessage && <span> - {secretMessage}</span>}
-      </p>
+      <>
       {!sessionData && (
-        <button
-          className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-          onClick={() => void signIn()}
+        // <button
+        //   className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+        //   onClick={() => void signIn()}
+        // >
+        //   Sign in
+        // </button>
+        <a
+        href="#"
+        className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
+        onClick={() => void signIn()}
+      >
+        Get started
+        <svg
+          aria-hidden="true"
+          className="ml-2 -mr-1 w-5 h-5"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
         >
-          Sign in
-        </button>
+          <path
+            fillRule="evenodd"
+            d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </a>
       )}
-    </div>
+    </>
   );
 };
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -26,44 +26,45 @@ export default function SignIn({
   providers,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
-    
-    <div className="flex min-h-screen items-center justify-center bg-gradient-to-b from-[#5215a8] to-[#2b33d7]">
-      <div className="w-full max-w-sm p-6 m-auto mx-auto bg-white rounded-lg shadow-md dark:bg-gray-800">
-  <div className="flex justify-center mx-auto my-4 text-xl font-extrabold font-sans">
-    Life on Rails
-  </div>
-  <div className="flex justify-center mx-auto my-2">
-    Sign Up or Log In
-  </div>
-  <hr className="w-48 h-1 mx-auto my-4 bg-gray-100 border-0 rounded dark:bg-gray-700"/>
 
-  <div className="flex flex-col items-center mt-6">
-    <button
-      type="button"
-      aria-label={`Continue with Google`}
-      role="button"
-      className="flex items-center justify-center w-3/4 px-6 py-2 m-2 text-sm font-medium text-white transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:bg-blue-400 focus:outline-none"
-      onClick={() => void signIn('google')}
-      
-    >
-      <BsGoogle className="w-4 h-4 mx-2 fill-current"></BsGoogle>
-      <span className="hidden mx-2 sm:inline">Continue with Google</span>
-    </button>
+    <div className="flex min-h-screen items-center justify-center bg-gradient-to-b from-[#5ef4ca5a] to-[#4389faab] "> 
+    {/* from-[#5215a8] to-[#2b33d7] */}
+      <div className="w-full max-w-sm p-6 m-auto mx-auto bg-gray-50 rounded-lg shadow-md dark:bg-gray-800">
+        <div className="flex justify-center mx-auto my-4 text-xl font-extrabold font-sans">
+          Life on Rails
+        </div>
+        <div className="flex justify-center mx-auto my-2">
+          Sign Up or Log In
+        </div>
+        <hr className="w-48 h-1 mx-auto my-4 bg-gray-200 border-0 rounded dark:bg-gray-700" />
 
-    <button
-      type="button"
-      aria-label={`Continue with Discord`}
-      role="button"
-      className="flex items-center justify-center w-3/4 px-6 py-2 m-2 text-sm font-medium text-white transition-colors duration-300 transform bg-gray-500 rounded-lg hover:bg-gray-400 focus:bg-gray-400 focus:outline-none"
-      onClick={() => void signIn('discord')}
-    >
-    <BsDiscord className="w-4 h-4 mx-2 fill-current"></BsDiscord>
-      <span className="hidden mx-2 sm:inline">Continue with Discord</span>
-    </button>
+        <div className="flex flex-col items-center mt-6">
+          <button
+            type="button"
+            aria-label={`Continue with Google`}
+            role="button"
+            className="flex items-center justify-center w-3/4 px-6 py-2 m-2 text-sm font-medium text-white transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:bg-blue-400 focus:outline-none"
+            onClick={() => void signIn('google')}
 
-  </div>
-  
-</div>
+          >
+            <BsGoogle className="w-4 h-4 mx-2 fill-current"></BsGoogle>
+            <span className="hidden mx-2 sm:inline">Continue with Google</span>
+          </button>
+
+          <button
+            type="button"
+            aria-label={`Continue with Discord`}
+            role="button"
+            className="flex items-center justify-center w-3/4 px-6 py-2 m-2 text-sm font-medium text-white transition-colors duration-300 transform bg-gray-500 rounded-lg hover:bg-gray-400 focus:bg-gray-400 focus:outline-none"
+            onClick={() => void signIn('discord')}
+          >
+            <BsDiscord className="w-4 h-4 mx-2 fill-current"></BsDiscord>
+            <span className="hidden mx-2 sm:inline">Continue with Discord</span>
+          </button>
+
+        </div>
+
+      </div>
 
     </div>
   );
