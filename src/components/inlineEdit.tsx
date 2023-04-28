e82639f4452b29b0025b7f3d830aba6573744498
Sumeet Padavala <sumeet.padavala@gmail.com>
2023-04-27 21:36:32 -0700

    Make inlineEdit components less shit

--- src/components/inlineEdit.tsx
+++ src/components/inlineEdit.tsx
@@ -1,13 +1,11 @@
-import { FrequencyHorizon } from "@prisma/client";
+import type { FrequencyHorizon } from "@prisma/client";
 import type { ChangeEvent, KeyboardEvent } from "react";
 import { useState } from "react";
 
 export const useInlineEdit = ({
-  placeholder,
   initialText,
   commit,
 }: {
-  placeholder: string;
   initialText: string;
   commit: (text: string) => void;
 }) => {
@@ -18,16 +16,21 @@ export const useInlineEdit = ({
     onClick: () => setActive(true),
   };
   const editProps = {
-    placeholder,
     value: text,
     onChange: (e: ChangeEvent<HTMLInputElement>) => setText(e.target.value),
+    onFocus: () => {
+      //setText(initialText);
+    },
+    onBlur: () => {
+      console.log("blur");
+      setActive(false);
+    },
     onKeyDown: (event: KeyboardEvent<HTMLInputElement>) => {
       if (event.key == "Enter") {
         commit(text);
         console.log(`Commit with text ${text}`);
         setActive(false);
       } else if (event.key == "Escape") {
-        setText("");
         setActive(false);
       }
     },
@@ -76,31 +79,27 @@ export const useInlineNumberEdit = ({
 };
 
 export const EditableField = ({
-  placeholder,
   initialText,
   commit,
 }: {
-  placeholder?: string;
   initialText: string;
   commit: (text: string) => void;
 }) => {
   const { isActive, triggerProps, editProps } = useInlineEdit({
-    placeholder: placeholder ?? "No placeholder",
     initialText,
     commit,
   });
 
   return (
-    <>
+    <div
+      className="group flex flex-row flex-nowrap gap-1 whitespace-nowrap"
+      {...triggerProps}
+    >
       {isActive ? (
         <input {...editProps} autoFocus className="font-bold" />
       ) : (
-        <div
-          className="group flex flex-row flex-nowrap gap-1 whitespace-nowrap"
-          {...triggerProps}
-        >
+        <>
           <span>{initialText}</span>
-
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -115,9 +114,9 @@ export const EditableField = ({
               d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
             />
           </svg>
-        </div>
+        </>
       )}
-    </>
+    </div>
   );
 };
 
