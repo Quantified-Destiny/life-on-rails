5909b0d3df1647be324a707021fc85141509f287
Sumeet Padavala <sumeet.padavala@gmail.com>
2023-04-27 23:00:16 -0700

    Refactor overview into tiny pieces

---
+++ src/components/overview/goals.tsx
@@ -0,0 +1,86 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import type { Metric } from "@prisma/client";
+import { EditableField } from "../../components/inlineEdit";
+import { HabitCard } from "../../components/overview/habits";
+import type {
+  ExpandedGoal,
+  ExpandedHabit,
+  ExpandedMetric,
+} from "../../server/queries";
+import { api } from "../../utils/api";
+import { TagList } from "./tags";
+
+export function GoalCard({
+  id,
+  name,
+  score,
+  habits,
+  metrics,
+  tags,
+}: ExpandedGoal & {
+  score: number;
+  habits: (ExpandedHabit & {
+    completions: number;
+    score: number;
+    metrics: ExpandedMetric[];
+  })[];
+  metrics: (Metric & { score: number })[];
+}) {
+  const context = api.useContext();
+  const mutation = api.goals.editGoal.useMutation({
+    onSettled: () => {
+      void context.goals.getGoals.invalidate();
+    },
+  });
+  const linkGoal = api.tags.linkGoal.useMutation({
+    onSettled: () => {
+      void context.goals.getGoals.invalidate();
+    },
+  });
+  const unlinkGoal = api.tags.linkGoal.useMutation({
+    onSettled: () => {
+      void context.goals.getGoals.invalidate();
+    },
+  });
+
+  return (
+    <div className="mb-8 rounded-lg bg-white p-6 shadow-md">
+      <div className="mb-1 flex flex-row justify-between">
+        <div className="mb-2 flex flex-col">
+          <span className="mb-2 inline-block h-fit w-fit rounded-full bg-yellow-500 px-2 py-1 text-xs font-bold text-white">
+            Goal
+          </span>
+          <div className="mb-2 flex items-center justify-between text-xl font-bold">
+            <EditableField
+              initialText={name}
+              commit={(name) => mutation.mutate({ goalId: id, name })}
+            ></EditableField>
+          </div>
+        </div>
+        <div className="h-fit w-fit rounded-lg bg-gray-100 p-2 text-xl font-bold text-yellow-500">
+          {score.toFixed(2)}
+        </div>
+      </div>
+      <div className=" gap-x-10 space-y-2">
+        {habits.map((habit) => (
+          <HabitCard
+            {...habit}
+            weight={0.4}
+            linkedGoal={id}
+            key={habit.id}
+          ></HabitCard>
+        ))}
+      </div>
+      <TagList
+        tags={tags}
+        link={() => {
+          (tag: string) => linkGoal.mutate({ goalId: id, tagName: tag });
+        }}
+        unlink={() => {
+          (tag: string) => unlinkGoal.mutate({ goalId: id, tagName: tag });
+        }}
+      ></TagList>
+    </div>
+  );
+}
---
+++ src/components/overview/metrics.tsx
@@ -0,0 +1,74 @@
+import classNames from "classnames";
+import { api } from "../../utils/api";
+import { DropdownMenu } from "../createMenu";
+import { EllipsisIcon } from "../icons";
+import { EditableField } from "../inlineEdit";
+import { textcolor } from "./lib";
+
+export function LinkedMetric({
+  id,
+  weight,
+  prompt,
+  score,
+}: {
+  id: string;
+  weight: number;
+  prompt: string;
+  score: number;
+}) {
+  const context = api.useContext();
+  const mutation = api.metrics.editMetric.useMutation({
+    onSuccess: () => {
+      void context.goals.getGoals.invalidate();
+    },
+  });
+  const deleteMetric = api.metrics.deleteMetric.useMutation({
+    onSuccess: () => {
+      void context.goals.getGoals.invalidate();
+    },
+  });
+
+  return (
+    <div className="m-2 flex min-h-[100px] flex-row justify-between rounded-lg bg-gray-100 p-4">
+      <div className="flex flex-col">
+        <div className="mb-2">
+          <span className="inline-block rounded-full bg-purple-500 px-2 py-1 text-xs font-bold text-white">
+            Linked Metric
+          </span>
+          <span className="text-gray mb-2 inline-block rounded-full px-2 text-xs font-bold">
+            Weight: {weight.toFixed(2)}
+          </span>
+        </div>
+        <div className="ml-2 font-semibold">
+          <EditableField
+            initialText={prompt}
+            commit={(text) => {
+              mutation.mutate({ metricId: id, prompt: text });
+            }}
+          ></EditableField>
+        </div>
+      </div>
+
+      <div className="flex h-full w-fit flex-row space-x-3">
+        <div className=" h-fit bg-white px-2">
+          <span
+            className={classNames("h-fit text-lg font-bold", textcolor(score))}
+          >
+            {score}
+          </span>
+        </div>
+        <DropdownMenu
+          options={[
+            {
+              name: "Delete",
+              onClick: () => deleteMetric.mutate({ metricId: id }),
+            },
+          ]}
+          trigger={
+            <EllipsisIcon className="fill-current text-gray-400 hover:text-gray-700"></EllipsisIcon>
+          }
+        ></DropdownMenu>
+      </div>
+    </div>
+  );
+}
