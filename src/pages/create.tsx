eb6b99ce5a97ff900b2bd9f94f378d0fa9db602b
Sumeet Padavala <sumeet.padavala@gmail.com>
2023-04-27 21:37:17 -0700

    Reformat stuff

--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -1,4 +1,3 @@
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import classNames from "classnames";
 import { useState } from "react";
@@ -38,27 +37,37 @@ const CreatePage = () => {
         <SubjectiveFormModal onClose={() => setIsSubjectiveModalOpen(false)} />
       )}
 
-
       <div className="mx-auto max-w-xl px-4 py-6 sm:px-6 lg:px-8">
         <h2 className="mb-8 text-2xl font-semibold">Choose an option:</h2>
         <div className="flex flex-col space-y-4">
           <button
             type="button"
             className="inline-flex items-center rounded-md border border-transparent bg-green-600 px-4 py-2 font-semibold text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2"
-            onClick={handleGoalModalToggle}>
+            onClick={handleGoalModalToggle}
+          >
             <span className="mr-2">
               <i className="fas fa-bullseye"></i>
             </span>
             <span>Create Goal</span>
           </button>
-          <button type="button" className="inline-flex items-center rounded-md border border-transparent bg-yellow-600 px-4 py-2 font-semibold text-white hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-offset-2"
-            onClick={handleHabitModalToggle}>
-            <span className="mr-2"><i className="fas fa-sticky-note"></i></span>
+          <button
+            type="button"
+            className="inline-flex items-center rounded-md border border-transparent bg-yellow-600 px-4 py-2 font-semibold text-white hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-offset-2"
+            onClick={handleHabitModalToggle}
+          >
+            <span className="mr-2">
+              <i className="fas fa-sticky-note"></i>
+            </span>
             <span>Create Habit</span>
           </button>
-          <button type="button" className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 font-semibold text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-            onClick={handleSubjectiveModalToggle}>
-            <span className="mr-2"><i className="fas fa-tasks"></i></span>
+          <button
+            type="button"
+            className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 font-semibold text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            onClick={handleSubjectiveModalToggle}
+          >
+            <span className="mr-2">
+              <i className="fas fa-tasks"></i>
+            </span>
             <span>Create Subjective</span>
           </button>
         </div>
@@ -67,4 +76,6 @@ const CreatePage = () => {
   );
 };
 
-export default () => <Layout main={CreatePage}></Layout>;
+export default function Page() {
+  return <Layout main={CreatePage}></Layout>;
+}
