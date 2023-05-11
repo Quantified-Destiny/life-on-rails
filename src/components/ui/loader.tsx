66a42acb26b173262cbb8688450b7953b7d1601a
Sumeet Padavala <sumeet.padavala@gmail.com>
2023-05-11 14:39:58 -0700

    Make the loader more reasonably sized

--- src/components/ui/loader.tsx
+++ src/components/ui/loader.tsx
@@ -3,7 +3,7 @@ import { RxRocket } from "react-icons/rx";
 export function Loader() {
   return (
     <div className="flex h-full w-full items-center justify-center">
-      <RxRocket className="h-52 w-52 animate-spin"></RxRocket>
+      <RxRocket className="h-24 w-24 animate-spin px-10"></RxRocket>
     </div>
   );
 }
