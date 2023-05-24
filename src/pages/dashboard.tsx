efa1f40a1cf6279ddbc8a24a88c25759eee59c0d
Brian Ha <26440404+beehaa@users.noreply.github.com>
2023-05-23 21:50:24 -0700

    charting and graphs for dashboard, placeholder

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -8,6 +8,66 @@ import { ScoringFormat } from "@prisma/client";
 import { api } from "../utils/api";
 import { PieChart, pieChartDefaultProps } from 'react-minimal-pie-chart';
 
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'bottom' as const,
+    },
+    title: {
+      display: true,
+      text: 'Number of Completions',
+    },
+  },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Habit Completions',
+      data: [0, 1, 2, 3, 4, 5, 8],
+      borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Metric Completions',
+      data: [3, 1, 5, 1, 5, 3, 2],
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
+
+// function App() {
+//   return <Line options={options} data={data} />;
+// }
+
+
 function min(a: number, b: number) {
   return a < b ? a : b;
 }
@@ -212,6 +272,7 @@ function StatsCardRow({
             />
           </div>
         </div>
+        <Line options={options} data={data} />
       </div>
     </div>
   );
