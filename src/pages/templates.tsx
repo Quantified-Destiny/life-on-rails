db62db52de852e5916096d981723b5b917eee1fa
Sumeet Padavala <sumeet.padavala@gmail.com>
2023-05-29 23:27:47 -0700

    Update templates page

--- src/pages/templates.tsx
+++ src/pages/templates.tsx
@@ -1,14 +1,12 @@
-import { InfoIcon } from "lucide-react";
+import { InfoIcon, Loader2 } from "lucide-react";
 import { useState } from "react";
-import {
-  Card,
-  CardHeader,
-  CardTitle
-} from "../components/ui/card";
+import { Card, CardHeader, CardTitle } from "../components/ui/card";
+import { useRouter } from "next/router";
 
 import Image from "next/image";
 import { Button } from "../components/ui/button";
 import { api } from "../utils/api";
+import { AiOutlineCheck } from "react-icons/ai";
 export const templates = [
   {
     key: "fitness",
@@ -28,29 +26,84 @@ export const templates = [
     key: "peace",
     title: "Peace",
     image: "/illustrations/mindfulness.svg",
-    description: "Make your life calmer and more mindful with these habits and goals.",
+    description:
+      "Make your life calmer and more mindful with these habits and goals.",
   },
 ];
 
-export const TemplatesPage = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false); // Step 1: State variable for modal open/closed
-  const [selectedTemplateKey, setSelectedTemplateKey] = useState(""); // New state variable for selected template key
+enum ModalState {
+  None,
+  Creating,
+  Done,
+}
 
-  const openModal = (key: string) => {
-    setSelectedTemplateKey(key); // Set the selected template key
-    setIsModalOpen(true);
-  };
+const actionButtonLabel = (state: ModalState) => {
+  switch (state) {
+    case ModalState.None:
+      return "Create";
+    case ModalState.Creating:
+      return <Loader2 className="h-6 w-6 animate-spin" />;
+    case ModalState.Done:
+      return <AiOutlineCheck />;
+  }
+};
 
-  const closeModal = () => {
-    setIsModalOpen(false); // Step 2: Function to close the modal
-  };
-
+const TemplateModal = ({
+  templateKey,
+  cancel,
+}: {
+  templateKey: string;
+  cancel: () => void;
+}) => {
+  const [state, setState] = useState<ModalState>(ModalState.None);
+  const router = useRouter();
   const context = api.useContext();
   const createFromTemplate = api.templates.createFromTemplate.useMutation({
     onSettled() {
       void context.invalidate();
     },
   });
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-75">
+      <div className="rounded-lg bg-white p-5">
+        <div className="text-md mb-5 font-semibold">
+          You are about to use the{" "}
+          <span className="bg-gray-200 font-mono">{templateKey}</span> template.{" "}
+        </div>
+        <div className="font-light text-gray-500">
+          This will create multiple goals, habits, and metrics. This operation
+          is irreversible.
+        </div>
+        <div className="mt-2 whitespace-nowrap align-baseline font-light text-gray-500">
+          <InfoIcon className="inline"></InfoIcon> You can delete these items
+          from the `All Items` page at any time.
+        </div>{" "}
+        <div className="mt-4 flex justify-end gap-x-2">
+          <Button variant="ghost" onClick={cancel}>
+            Cancel
+          </Button>
+
+          <Button
+            variant="default"
+            onClick={async () => {
+              setState(ModalState.Creating);
+              await createFromTemplate.mutateAsync({ key: templateKey });
+              setState(ModalState.Done);
+              await router.push({ pathname: "/overview" });
+            }}
+          >
+            {actionButtonLabel(state)}
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const TemplatesPage = () => {
+  const [templateKey, setTemplateKey] = useState<string | undefined>(undefined); // New state variable for selected template key
+
   return (
     <div className="container mx-auto max-w-screen-lg px-4 py-8">
       <h1 className="my-5 text-2xl font-bold">Templates</h1>
@@ -65,7 +118,7 @@ export const TemplatesPage = () => {
               className="h-[156px] w-2/3 cursor-pointer hover:shadow-md"
               key={template.key}
               // onClick={() => createFromTemplate.mutate({ key: template.key })}
-              onClick={() => openModal(template.key)}
+              onClick={() => setTemplateKey(template.key)}
             >
               <div className="relative flex h-full flex-row items-center">
                 <div className="flex h-[156px] w-[156px] flex-col items-center justify-center bg-gray-200 text-white">
@@ -87,44 +140,11 @@ export const TemplatesPage = () => {
           );
         })}
       </div>
-      {isModalOpen && (
-        <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-75">
-          <div className="rounded-lg bg-white p-5">
-            <div className="text-md mb-5 font-semibold">
-              You are about to use the{" "}
-              <span className="bg-gray-200 font-mono">
-                {selectedTemplateKey}
-              </span>{" "}
-              template.{" "}
-            </div>
-            <div className="font-light text-gray-500">
-              This will create multiple goals, habits, and metrics. This
-              operation is irreversible.
-            </div>
-            <div className="mt-2 whitespace-nowrap align-baseline font-light text-gray-500">
-              <InfoIcon className="inline"></InfoIcon> You can delete these
-              items from the `All Items` page at any time.
-            </div>{" "}
-            <div className="mt-4 flex justify-end gap-x-2">
-              <Button variant="ghost" onClick={closeModal}>
-                Cancel
-              </Button>
-
-              <Button
-                variant="default"
-                onClick={() => {
-                  // Perform the goal creation action here
-                  if (selectedTemplateKey) {
-                    createFromTemplate.mutate({ key: selectedTemplateKey });
-                  }
-                  closeModal(); // Close the modal after confirming
-                }}
-              >
-                Confirm
-              </Button>
-            </div>
-          </div>
-        </div>
+      {templateKey && (
+        <TemplateModal
+          templateKey={templateKey}
+          cancel={() => setTemplateKey(undefined)}
+        ></TemplateModal>
       )}
     </div>
   );
