54e1562f726ebaa0ac09fb7b20c1664a564708c9
Sumeet Padavala <sumeet.padavala@gmail.com>
2023-06-14 02:00:09 -0700

    I fucking hate clerk

---
+++ src/components/authbox.tsx
@@ -0,0 +1,64 @@
+import {
+    SignedInAuthObject,
+    SignedOutAuthObject,
+} from "@clerk/nextjs/api";
+import { getAuth } from "@clerk/nextjs/server";
+import { GetServerSideProps } from "next";
+import Link from "next/link";
+
+export const AuthShowcase = () => {
+  //const { isLoaded, isSignedIn, user } = useUser();
+  
+  const isSignedIn = true;
+  
+  if (isSignedIn) {
+    return (
+      <>
+        <Link
+          href="/journal"
+          className="inline-flex items-center justify-center rounded-lg bg-blue-700 px-5 py-3 text-center text-base font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
+        >
+          Go to app
+          <svg
+            aria-hidden="true"
+            className="-mr-1 ml-2 h-5 w-5"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </Link>
+      </>
+    );
+  }
+  return (
+    <>
+      <Link
+        href="/sign-in"
+        className="inline-flex items-center justify-center rounded-lg bg-blue-700 px-5 py-3 text-center text-base font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
+      >
+        Login
+        <svg
+          aria-hidden="true"
+          className="-mr-1 ml-2 h-5 w-5"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </Link>
+    </>
+  );
+};
+
+export default AuthShowcase;
