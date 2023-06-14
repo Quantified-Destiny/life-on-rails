8dfaca73dff2052b7badbc46931ef3c3de503a44
Sumeet Padavala <sumeet.padavala@gmail.com>
2023-06-14 15:04:10 -0700

    Tweak index a bit

--- src/components/authbox.tsx
+++ src/components/authbox.tsx
@@ -1,56 +1,52 @@
-import { useUser } from "@clerk/nextjs";
+import { useAuth } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
 import Link from "next/link";
 
+const RightArrow = () => (
+  <svg
+    aria-hidden="true"
+    className="-mr-1 ml-2 h-5 w-5"
+    fill="currentColor"
+    viewBox="0 0 20 20"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fillRule="evenodd"
+      d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+
 export const AuthShowcase = () => {
-  const { isLoaded, isSignedIn, user } = useUser();
-  
+  const { isLoaded, isSignedIn } = useAuth();
+
+  if (!isLoaded) {
+    return (
+      <div className="inline-flex items-center justify-center rounded-lg bg-blue-700 px-5 py-3 text-center text-base font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+        <Loader />
+      </div>
+    );
+  }
   if (isSignedIn) {
     return (
-      <>
-        <Link
-          href="/journal"
-          className="inline-flex items-center justify-center rounded-lg bg-blue-700 px-5 py-3 text-center text-base font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
-        >
-          Go to app
-          <svg
-            aria-hidden="true"
-            className="-mr-1 ml-2 h-5 w-5"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </Link>
-      </>
+      <Link
+        href="/journal"
+        className="inline-flex items-center justify-center rounded-lg bg-blue-700 px-5 py-3 text-center text-base font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
+      >
+        Go to app
+        <RightArrow />
+      </Link>
     );
   }
   return (
-    <>
-      <Link
-        href="/sign-in"
-        className="inline-flex items-center justify-center rounded-lg bg-blue-700 px-5 py-3 text-center text-base font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
-      >
-        Login
-        <svg
-          aria-hidden="true"
-          className="-mr-1 ml-2 h-5 w-5"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fillRule="evenodd"
-            d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-            clipRule="evenodd"
-          />
-        </svg>
-      </Link>
-    </>
+    <Link
+      href="/sign-in"
+      className="inline-flex items-center justify-center rounded-lg bg-blue-700 px-5 py-3 text-center text-base font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
+    >
+      Login
+      <RightArrow />
+    </Link>
   );
 };
 
